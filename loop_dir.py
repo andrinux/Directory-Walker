3608583ea46393f2b766ba1925d2e39528b3b2e7
Andrinux Loid <andrinux.loid@gmail.com>
2015-04-28 18:23:08 -0400

    Print all the subfolders recursively by level

---
+++ loop_dir.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import commands
+
+def display(path, level):
+  #Try the compression here and get the average compression ratio
+  for i in range(0, level):
+    print ' ',
+  if(os.path.isdir(path)):
+    print('d:'),
+  elif(os.path.isfile(path)):
+    print('r:'),
+  else:
+    print('o:'),
+  print( str(level)+ ':'+ path + ':'),
+  (status,output)=commands.getstatusoutput('./Comp res.txt <'+path);
+  if(os.path.isdir(path)):
+    print ''
+  elif(os.path.isfile(path)):
+    print output+''
+  else:
+      print ''
+
+def LoopDir(rootDir,level):   
+  for lists in os.listdir(rootDir):       
+    path = os.path.join(rootDir, lists)
+    if os.path.isdir(path):
+      display(path, level)
+      LoopDir(path,level+1)
+    else:
+      display(path, level)
+
+rootDir='/media/Seagate/DataSet/5.0_50/data/media/obb/com.dirtybit.funrun2'
+level=0
+LoopDir(rootDir,level)
