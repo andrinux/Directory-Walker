4bb2b84f8c404c5415f0a9baf482699c8911000d
iAndrinux <andrinux.loid@gmail.com>
2015-08-20 19:31:58 -0700

    multi thread usage of Telnet in Perl

---
+++ multiThread.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+# Verified on Windows 8
+# Need to install Net::Telnet module
+#
+use strict;
+use Net::Telnet;
+
+use constant Timeout =>'5';
+use constant USER => 'XuebinZhang';
+use constant PASS => '19900701';
+
+my $pid = fork();
+
+if (not defined $pid) {
+	print "resources not avilable. ";
+} 
+elsif ($pid == 0) {
+	print "This is the child process.\n";
+	sleep 1;
+	#Run telnet
+	my $telnet=Net::Telnet->new(
+        Timeout =>10,
+        Prompt =>'/./',
+        host =>'128.113.224.116'
+        );
+	print "Perl Telnet Test on W530.\n";
+
+	$telnet->dump_log('log');
+	$telnet->login(USER,PASS);
+	$telnet->waitfor('/\>/');#Wait for cmd charater
+	my @lines=$telnet->cmd("ipconfig");
+	print @lines;
+
+	#end telnet
+	exit(0);
+} else {
+	print "IM THE Parent Process.\n";
+	waitpid($pid,1);
+}
+	print "END ALL Process.";
